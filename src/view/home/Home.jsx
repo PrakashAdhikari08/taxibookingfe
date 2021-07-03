cdca9e21570c3f5bba9ecba6f785b98d239cc8d7
PrakashAdhikari08 <prakashadhikarijhapali@gmail.com>
2021-07-03 12:37:01 +1000

    home screen background added

--- src/view/home/Home.jsx
+++ src/view/home/Home.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import './Home.css';
+import Footer from "../footer/Footer";
 
 
 const Home = (props) => {
 
     return(
-        <div>
+        <div className={"text-center home_main"}>
 
             <h1>Hello From Home</h1>
+            <h1>Hello From Home</h1>
+            <h1>Hello From Home</h1>
+            <h1>Hello From Home</h1>
+            <h1>Hello From Home</h1>
+
+            <h1>Hello From Home</h1>
+            <h1>Hello From Home</h1>
+            <h1>Hello From Home</h1>
+
+
         </div>
     )
 }
